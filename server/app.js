abce4cf71da617387429ec055f585bf1c1b5e400
Sam <smorrow@oberlin.edu>
2018-06-27 18:04:47 -0400

    socket prototype

--- server/app.js
+++ server/app.js
@@ -2,38 +2,11 @@ const path = require('path');
 const express = require('express')
 const morgan = require('morgan');
 const session = require('express-session');
-
 const PORT = process.env.PORT || 8080;
 const app = express()
-// var cookieSession = require('cookie-session')
-
-// const db = require('./db')
-
-// db.set('my test key', JSON.stringify('test'))
-// db.get('my test key', function(error, result) {
-//   if (error) throw error;
-//   console.log('GET result ->', result)
-// });
 
 app.use(morgan('dev'))
-// .use(cookieSession({
-//   name: 'session',
-//   keys: ['my dog is Cody'],
-//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
-// }))
-// .use(function (req, res, next) {
-//   req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
-//   next()
-// })
 .use(express.static(path.join(__dirname, '..', 'public')))
-// .use(session({
-//   secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-//   resave: false,
-//   saveUninitialized: true,
-//   maxAge: 60000
-// }))
-// .use(passport.initialize())
-// .use(passport.session())
 .use((req, res, next) =>
   path.extname(req.path).length > 0 ? res.status(404).send('Not found') : next())
 .use('*', (req, res) =>
@@ -52,6 +25,6 @@ server.on('connection', function(socket){
 });
 
 
-http.listen(8080, () => {
-  console.log('listening on port 8080')
+http.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,7 +1,6 @@
 export default {
   send(...args) {
     let msg = JSON.stringify(args)
-    console.log("well", this)
     this.$store.state.ws.send(msg)
   }
 }
